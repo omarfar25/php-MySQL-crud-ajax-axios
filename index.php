51e4c5d5dbfd1c9bdbc1fc91d0f36bd926bd519f
Omar <omarfarq143@gmail.com>
2020-08-22 18:02:07 +0600

    some more cmnt

--- index.php
+++ index.php
@@ -124,9 +124,9 @@ function editpage(id) {
       <div class="alert alert-success alert-dismissible fade show" role="alert">
         <strong>data add success!</strong>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span> -->
+          <span aria-hidden="true">&times;</span>
         </button>
-      </div>
+      </div> -->
     <table class="table">
       <thead class="thead-dark">
         <tr>
